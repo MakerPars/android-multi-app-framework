139d56b624405e9cc5c63bd6dfb7101788ad3aa1
MakerPars <MakerPars@gmail.com>
2026-03-06 21:05:00 +0300

    Restore Firebase Hosting rewrite exports

--- side-projects/firebase/functions/src/index.ts
+++ side-projects/firebase/functions/src/index.ts
@@ -7,14 +7,12 @@ admin.initializeApp();
 // MOVED to direct Firestore SDK writes from Android client (FirestorePushRegistrationSender)
 // export { registerDevice } from "./registerDevice";
 export { dispatchNotifications } from "./dispatchNotifications";
-// MOVED to Cloudflare Worker (contentapp-content-api /api/other-apps with icon enrichment)
-// export { otherAppsFeed } from "./otherAppsFeed";
+export { otherAppsFeed } from "./otherAppsFeed";
 export { sendTestNotification } from "./sendTestNotification";
 export { deviceCoverageReport } from "./deviceCoverageReport";
 export { adPerformance, generateAdPerformanceWeeklyReport } from "./adPerformanceReport";
 export { adminAccessCheck } from "./adminAccessCheck";
 export { adminGetRemoteConfig, adminUpdateRemoteConfig } from "./adminRemoteConfig";
 export { healthCheck } from "./healthCheck";
-// MOVED to Cloudflare Worker (contentapp-content-api /api/recaptcha-verify)
-// export { recaptchaVerify } from "./recaptchaVerify";
+export { recaptchaVerify } from "./recaptchaVerify";
 export { verifyPurchase } from "./verifyPurchase";
